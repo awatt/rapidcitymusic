447b96ac3ab08a8c9fbc802633accfaf4a8ee030
Andrew Watt <awatt11215@gmail.com>
2016-10-29 10:54:55 -0400

    reinstall dependencies for deploy

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var path = require('path');
 var express = require('express');
-var webpack = require('webpack');
-var webpackMiddleware = require('webpack-dev-middleware');
-var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config.js');
+// var webpack = require('webpack');
+// var webpackMiddleware = require('webpack-dev-middleware');
+// var webpackHotMiddleware = require('webpack-hot-middleware');
+// var config = require('./webpack.config.js');
 var bodyParser = require('body-parser');
 
-var isDeveloping = process.env.NODE_ENV !== 'production';
-var port = isDeveloping ? 3000 : process.env.PORT;
+// var isDeveloping = process.env.NODE_ENV !== 'production';
+// var port = isDeveloping ? 3000 : Number(process.env.PORT);
 var app = express();
+var port = Number(process.env.PORT) || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,44 +18,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var email = require('./routes/email');
 app.use('/email', email);
 
-if (isDeveloping) {
-	var compiler = webpack(config);
+// if (isDeveloping) {
+// 	var compiler = webpack(config);
 
-	  compiler.plugin('compile', function() {
-	    console.log('Bundling...');
-	    bundleStart = Date.now();
-	  });
+// 	  compiler.plugin('compile', function() {
+// 	    console.log('Bundling...');
+// 	    bundleStart = Date.now();
+// 	  });
 
-	  compiler.plugin('done', function() {
-	    console.log('Bundled in ' + (Date.now() - bundleStart) + 'ms!');
-	  });		
+// 	  compiler.plugin('done', function() {
+// 	    console.log('Bundled in ' + (Date.now() - bundleStart) + 'ms!');
+// 	  });		
 
-	var middleware = webpackMiddleware(compiler, {
-		publicPath: config.output.publicPath,
-		contentBase: 'src',
-		stats: {
-			colors: true,
-			hash: false,
-			timings: true,
-			chunks: false,
-			chuckModuels: false,
-			modules: false
-		}
-	});
+// 	var middleware = webpackMiddleware(compiler, {
+// 		publicPath: config.output.publicPath,
+// 		contentBase: 'src',
+// 		stats: {
+// 			colors: true,
+// 			hash: false,
+// 			timings: true,
+// 			chunks: false,
+// 			chuckModuels: false,
+// 			modules: false
+// 		}
+// 	});
 
-	app.use(middleware);
-	app.use(webpackHotMiddleware(compiler));
-	app.get('*', function response(req, res){
-		res.sendFile(path.join(__dirname, 'index_dev.html'));
-	});
+// 	app.use(middleware);
+// 	app.use(webpackHotMiddleware(compiler));
+// 	app.get('*', function response(req, res){
+// 		res.sendFile(path.join(__dirname, 'index_dev.html'));
+// 	});
 
-} else {
-	console.log("GOT INTO BUILD PROCESS")
-	app.use(express.static(__dirname + '/dist'));
-	app.get('*', function response(req, res){
-		res.sendFile(path.join(__dirname, '/dist/index.html'));
-	});
-}
+// } else {
+// 	app.use(express.static(__dirname + '/dist'));
+// 	app.get('*', function response(req, res){
+// 		res.sendFile(path.join(__dirname, '/dist/index.html'));
+// 	});
+// }
+
+
+app.use(express.static(__dirname + '/dist'));
+app.get('*', function response(req, res){
+	res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 
 app.listen(port, '0.0.0.0', function onStart (err) {
 	if(err){
